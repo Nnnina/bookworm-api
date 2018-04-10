9f42b3c9745901c890498130b2694e5bac01807f
Nnnina <nnninalei@gmail.com>
2018-04-10 15:10:38 -0400

    mongodb added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import Promise from "bluebird";
 
+import auth from "./routes/auth";
+//import users from "./routes/users";
+//import books from "./routes/books";
+
+dotenv.config();
 const app = express();
+app.use(bodyParser.json());
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/bookworm", { useMongoClient: true });
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.use("/api/auth", auth);
+//app.use("/api/users", users);
+//app.use("/api/books", books);
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({ errors: { global: "Invalid Credentials" }});
-})
-
-
-app.listen(8080, () => {
-    console.log("Running on localhost:8080");
-});
+app.listen(8080, () => console.log("Running on localhost:8080"));
